9304de484118af76d7724d899f101f3dc2d28961
weichu <yangtfei204@gmail.com>
2026-06-21 21:47:16 +0800

    🐾 BiliPlayer 🐾
    酒馆内嵌式视频播放器
    ❤️致谢：感谢 [老农民] 老师提供最底层的视频加载逻辑与播放器基础代码。(｀･ω･´)ゞ(｀･ω･´)ゞ

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
  * 🐾 BiliPlayer Extension for SillyTavern
  * 
  * Author: 未初
+ * Version: 2.0.0
  * 💖 Special Thanks: 本插件的底层基础逻辑与核心机制由【老农民】老师提供支持，特此致谢！
  */
 
@@ -1159,7 +1160,7 @@ function openHelpDialog() {
                 </div>
 
                 <div class="bili-help-footer">
-                    🐾 BiliPlayer v1.2.0 · 让小主人看电影更开心~<br>
+                    🐾 BiliPlayer v2.0.0 · 让小主人看电影更开心~<br>
                     <span style="font-size:10px; opacity:0.8;">💖 特别鸣谢：本插件底层核心逻辑由 <b>老农民</b> 老师提供支持</span>
                 </div>
             </div>
